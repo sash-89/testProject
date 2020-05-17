27128c9c9ab4eb8a91df4697dbe2e4853c92796b
sash-89 <sash060289@gmail.com>
2020-05-18 03:20:04 +0400

    Updates

---
+++ precache-manifest.5c3ef8e16f8499c9174ae1ac658d9b18.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5991415307767120b60241d11587c1d3",
+    "url": "/testProject/index.html"
+  },
+  {
+    "revision": "0aedc6ca5721790af30e",
+    "url": "/testProject/static/css/main.992ef55f.chunk.css"
+  },
+  {
+    "revision": "5bd9eeb6610b099211ad",
+    "url": "/testProject/static/js/2.245b8b2b.chunk.js"
+  },
+  {
+    "revision": "22a8d991fcab984b1d92e8ba9eb7dda0",
+    "url": "/testProject/static/js/2.245b8b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aedc6ca5721790af30e",
+    "url": "/testProject/static/js/main.541591ad.chunk.js"
+  },
+  {
+    "revision": "45d685cca3f25c6b0088",
+    "url": "/testProject/static/js/runtime-main.59232b16.js"
+  },
+  {
+    "revision": "0329f97bad81f7ddf2bf7cf83c9bf9f8",
+    "url": "/testProject/static/media/close.0329f97b.png"
+  },
+  {
+    "revision": "029ca5679fec1963ff8731989e3098de",
+    "url": "/testProject/static/media/img_194863.029ca567.png"
+  },
+  {
+    "revision": "db0cc9ac74a14a8ef2d6b2be1aa84bad",
+    "url": "/testProject/static/media/img_401107.db0cc9ac.png"
+  },
+  {
+    "revision": "c942fb22b42498355d8a47191feea9b6",
+    "url": "/testProject/static/media/img_504586.c942fb22.png"
+  },
+  {
+    "revision": "e7d7b20b36594ed86216efb8bede825a",
+    "url": "/testProject/static/media/likemessage_81127.e7d7b20b.ico"
+  },
+  {
+    "revision": "dc071cae726d46e5e50a377ae4fc77bf",
+    "url": "/testProject/static/media/no-drop.dc071cae.cur"
+  },
+  {
+    "revision": "714222f600c14de47fcf5cf5f9e1068b",
+    "url": "/testProject/static/media/preload.714222f6.gif"
+  },
+  {
+    "revision": "f9f7722e665fd4b50b49942c38cc1c62",
+    "url": "/testProject/static/media/thermometer-with-high-temperature_icon-icons.com_54634.f9f7722e.ico"
+  },
+  {
+    "revision": "8a5fd237cf1c85130f9982f8c32a7e8e",
+    "url": "/testProject/static/media/wind_120368.8a5fd237.ico"
+  }
+]);
